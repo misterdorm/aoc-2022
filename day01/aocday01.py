74dbfe263963ccd5ffda7d72cd772b4ddbf67851
Mike Dorman <mdorman@twilio.com>
2022-12-05 17:32:55 -0700

    Part 1 solved

---
+++ day01/aocday01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+elf_calories = [0]
+elf = 0
+
+for line in map(str.rstrip, sys.stdin):
+    print(line)
+    try:
+        elf_calories[elf] = elf_calories[elf] + int(line)
+    except ValueError:
+        print("Elf " + str(elf) + ": " + str(elf_calories[elf]))
+        elf = elf + 1
+        elf_calories.append(0)
+
+elf_calories.sort(reverse=True)
+
+print('---------------')
+print(elf_calories[0])

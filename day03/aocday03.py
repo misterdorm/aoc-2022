e4137200db961aab7352ebe36e5206b0fa86f3ca
Mike Dorman <mdorman@twilio.com>
2022-12-06 17:54:04 -0700

    solved day 3, part 2

--- day03/aocday03.py
+++ day03/aocday03.py
@@ -2,23 +2,22 @@
 
 import sys
 
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
 sum = 0
 
-for line in map(str.rstrip, sys.stdin):
-    c1 = line[:len(line)//2]
-    c2 = line[len(line)//2:]
-    print(c1 + ", " + c2)
+for lines in chunker(list(map(str.rstrip, sys.stdin)), 3):
+    print(lines)
+    item = ''.join(set(lines[0]).intersection(lines[1]).intersection(lines[2]))
+    print(item)
 
-    type = ''.join(
-        set(c1).intersection(c2)
-        )
-
-    if type.islower():
-        p = ord(type) - 96
+    if item.islower():
+        p = ord(item) - 96
     else:
-        p = ord(type) - 38
+        p = ord(item) - 38
 
-    print(type + ": " + str(ord(type)) + " --> " + str(p))
+    print(item + ": " + str(ord(item)) + " --> " + str(p))
     sum = sum + p
 
 print(sum)

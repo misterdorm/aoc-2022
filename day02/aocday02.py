9808a6da0aabc19ba377791941c204d00b9796ec
Mike Dorman <mdorman@twilio.com>
2022-12-06 17:26:16 -0700

    solved day 2 part 2

--- day02/aocday02.py
+++ day02/aocday02.py
@@ -2,17 +2,28 @@
 
 import sys
 
-# A = X = ROCK
-# B = Y = PAPER
-# C = Z = SCISSORS
+# A = ROCK = X
+# B = PAPER = Y
+# C = SCISSORS = Z
+
+# X = lose
+# Y = draw
+# Z = win
 
 score_map = { 'X': 1, 'Y': 2, 'Z': 3 }
+pick_map = {
+    'X': { 'A': 'Z', 'B': 'X', 'C': 'Y' },
+    'Y': { 'A': 'X', 'B': 'Y', 'C': 'Z' },
+    'Z': { 'A': 'Y', 'B': 'Z', 'C': 'X' }
+}
 match_map = { 'X': 'A', 'Y': 'B', 'Z': 'C' }
 beats_map = { 'X': 'C', 'Y': 'A', 'Z': 'B' }
 score = 0
 
 for line in map(str.rstrip, sys.stdin):
     shapes = line.split()
+    print(shapes)
+    shapes[1] = pick_map[shapes[1]][shapes[0]]
     print(shapes)
     score = score + score_map[shapes[1]]
     print(score)

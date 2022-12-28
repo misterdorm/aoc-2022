33bf908d2f09d6fb230b6e87cd725b2c55e81c32
Mike Dorman <mdorman@twilio.com>
2022-12-27 17:23:33 -0700

    solved day 9 part 2

--- day09/aocday09.go
+++ day09/aocday09.go
@@ -8,17 +8,17 @@ import (
     "strings"
 )
 
-type MoveFunc func([2]int, [2]int, int)([2]int, [2]int)
-
 var locs map[string]bool
+var knot_count = 10
+
+type MoveFunc func([][2]int, int)([][2]int)
 
 func main() {
   moves := map[string]MoveFunc{"R":R, "L":L, "U":U, "D":D}
-  head := [2]int{0,0}
-  tail := [2]int{0,0}
+  knots := make([][2]int, knot_count)
   locs = map[string]bool{}
 
-  record_tail(tail)
+  record_tail(knots[9])
 
   reader := bufio.NewReader(os.Stdin)
 
@@ -34,59 +34,89 @@ func main() {
     move := moves[line[0:1]]
     dist, _ := strconv.Atoi(line[2:])
 
-    head, tail = move(head, tail, dist)
+    knots = move(knots, dist)
+    record_tail(knots[knot_count-1])
 
-    fmt.Printf("move %s by %d, now at h(%d,%d) t(%d,%d)\n", line[0:1], dist, head[0], head[1], tail[0], tail[1])
+    fmt.Printf("move %s by %d\n", line[0:1], dist)
+    dump_knots(knots)
   }
 
   fmt.Printf("total locations visited by tail: %d\n", len(locs))
 }
 
-func R(h [2]int, t [2]int, d int) ([2]int, [2]int) {
+func R(k [][2]int, d int) [][2]int {
   fmt.Printf("+++ R %d\n", d)
   for i := 0; i < d; i++ {
-    h[0] = h[0] + 1
-    if(!is_touching(h, t)) { t = move_tail(h, t) }
+    k[0][0] = k[0][0] + 1
+    for j := 0; j < knot_count - 1; j++ {
+      //fmt.Printf("~~~ checking knots %d and %d\n", j, j+1)
+      //dump_knots(k)
+      if(!is_touching(k[j], k[j+1])) {
+        k[j+1] = move_tail(k[j], k[j+1])
+      }
+      record_tail(k[knot_count-1])
+    }
   }
-  return h, t
+  return k
 }
 
-func L(h [2]int, t [2]int, d int) ([2]int, [2]int) {
+func L(k [][2]int, d int) [][2]int {
   fmt.Printf("+++ L %d\n", d)
   for i := 0; i < d; i++ {
-    h[0] = h[0] - 1
-    if(!is_touching(h, t)) { t = move_tail(h, t) }
+    k[0][0] = k[0][0] - 1
+    for j := 0; j < knot_count - 1; j++ {
+      //fmt.Printf("~~~ checking knots %d and %d\n", j, j+1)
+      //dump_knots(k)
+      if(!is_touching(k[j], k[j+1])) {
+        k[j+1] = move_tail(k[j], k[j+1])
+      }
+      record_tail(k[knot_count-1])
+    }
   }
-  return h, t
+  return k
 }
 
-func U(h [2]int, t [2]int, d int) ([2]int, [2]int) {
+func U(k [][2]int, d int) [][2]int {
   fmt.Printf("+++ U %d\n", d)
   for i := 0; i < d; i++ {
-    h[1] = h[1] + 1
-    if(!is_touching(h, t)) { t = move_tail(h, t) }
+    k[0][1] = k[0][1] + 1
+    for j := 0; j < knot_count - 1; j++ {
+      //fmt.Printf("~~~ checking knots %d and %d\n", j, j+1)
+      //dump_knots(k)
+      if(!is_touching(k[j], k[j+1])) {
+        k[j+1] = move_tail(k[j], k[j+1])
+      }
+      record_tail(k[knot_count-1])
+    }
   }
-  return h, t
+  return k
 }
 
-func D(h [2]int, t [2]int, d int) ([2]int, [2]int) {
+func D(k [][2]int, d int) [][2]int {
   fmt.Printf("+++ D %d\n", d)
   for i := 0; i < d; i++ {
-    h[1] = h[1] - 1
-    if(!is_touching(h, t)) { t = move_tail(h, t) }
+    k[0][1] = k[0][1] - 1
+    for j := 0; j < knot_count - 1; j++ {
+      //fmt.Printf("~~~ checking knots %d and %d\n", j, j+1)
+      //dump_knots(k)
+      if(!is_touching(k[j], k[j+1])) {
+        k[j+1] = move_tail(k[j], k[j+1])
+      }
+      record_tail(k[knot_count-1])
+    }
   }
-  return h, t
+  return k
 }
 
 func is_touching(h [2]int, t [2]int) bool {
-  fmt.Printf("--- h(%d,%d) t(%d,%d) ", h[0], h[1], t[0], t[1])
+  //fmt.Printf("--- h(%d,%d) t(%d,%d) ", h[0], h[1], t[0], t[1])
 
   // If we are 1 or less distant on both dimensions, we are touching
   if(math.Abs(float64(h[0] - t[0])) <= 1 && math.Abs(float64(h[1] - t[1])) <= 1) {
-    fmt.Println("touching")
+    //fmt.Println("touching")
     return true
   }
-  fmt.Printf("NOT touching ")
+  //fmt.Printf("NOT touching ")
   return false
 }
 
@@ -108,31 +138,41 @@ func move_tail(h [2]int, t [2]int) [2]int {
     // else tail is right of head, move tail to just right of head
     if(t[0] > h[0]) { t[0] = h[0] + 1 }
   } else {
-    // neither x nor y match, diagonal
-    // figure out which dimension has the bigger gap
-    if(math.Abs(float64(h[0] - t[0])) - math.Abs(float64(h[1] - t[1])) > 0) {
-      // x delta is larger than y delta
-      // set y's to equal, x to one offset
-      t[1] = h[1]
-      // if tail is left of head, move tail to just left of head
-      if(t[0] < h[0]) { t[0] = h[0] - 1 }
-
-      // else tail is right of head, move tail to just right of head
-      if(t[0] > h[0]) { t[0] = h[0] + 1 }
+    // neither x nor y match, move diagonally toward head
+    if(h[0] > t[0]) {
+      t[0]++
     } else {
-      // y delta is larger than x delta
-      // set x's to equal, y to one offset
-      t[0] = h[0]
-      // if tail is under head, move tail up to just under head
-      if(t[1] < h[1]) { t[1] = h[1] - 1 }
-
-      // else tail is over head, move tail to just over head
-      if(t[1] > h[1]) { t[1] = h[1] + 1 }
+      t[0]--
     }
+
+    if(h[1] > t[1]) {
+      t[1]++
+    } else{
+      t[1]--
+    }
+
+    // if(math.Abs(float64(h[0] - t[0])) - math.Abs(float64(h[1] - t[1])) > 0) {
+    //   // x delta is larger than y delta
+    //   // set y's to equal, x to one offset
+    //   t[1] = h[1]
+    //   // if tail is left of head, move tail to just left of head
+    //   if(t[0] < h[0]) { t[0] = h[0] - 1 }
+    //
+    //   // else tail is right of head, move tail to just right of head
+    //   if(t[0] > h[0]) { t[0] = h[0] + 1 }
+    // } else {
+    //   // y delta is larger than x delta
+    //   // set x's to equal, y to one offset
+    //   t[0] = h[0]
+    //   // if tail is under head, move tail up to just under head
+    //   if(t[1] < h[1]) { t[1] = h[1] - 1 }
+    //
+    //   // else tail is over head, move tail to just over head
+    //   if(t[1] > h[1]) { t[1] = h[1] + 1 }
+    // }
   }
 
-  fmt.Printf("--> t(%d, %d)\n", t[0], t[1])
-  record_tail(t)
+  //fmt.Printf("--> t(%d, %d)\n", t[0], t[1])
   return t
 }
 
@@ -140,4 +180,37 @@ func record_tail(t [2]int) {
   idx := fmt.Sprintf("%d,%d", t[0], t[1])
   fmt.Printf("**** recording index: %s\n", idx)
   locs[idx] = true
+}
+
+func dump_knots(k [][2]int) {
+  x_max := 0
+  x_min := 0
+  y_max := 0
+  y_min := 0
+
+  for i := 0; i < len(k); i++ {
+    if(k[i][0] > x_max) { x_max = k[i][0] }
+    if(k[i][0] < x_min) { x_min = k[i][0] }
+    if(k[i][1] > y_max) { y_max = k[i][1] }
+    if(k[i][1] < y_min) { y_min = k[i][1] }
+  }
+
+  p := make([][]string, y_max - y_min + 1)
+  for i := range p {
+    p[i] = make([]string, x_max - x_min + 1)
+  }
+
+  for i := 0; i < len(k); i++ {
+    p[k[i][1] - y_min][k[i][0] - x_min] = fmt.Sprintf("%d", i)
+  }
+
+  fmt.Println("=============================")
+  for i := len(p) - 1; i >= 0; i-- {
+    for j := range p[i] {
+      if(p[i][j] == "") { p[i][j] = "." }
+      fmt.Printf("%s", p[i][j])
+    }
+    fmt.Println("")
+  }
+  fmt.Println("=============================")
 }
